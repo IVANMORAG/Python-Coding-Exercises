f69e62ff48b6806e1c70e4ef3cc31f3f6ecfcd62
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-03-20 14:48:17 -0600

    feat: add 01_Ejercicio

---
+++ 01_Introduccion/1.1_Arithmetic_Operators/01_Ejercicio.py
@@ -0,0 +1,11 @@
+"""
+EJERCICIO 1:
+Calcula el resultado de: 23 + 45 * 2 - 8 // 3.
+
+Descripción: Resuelve la expresiónrespetando la 
+procedencia de operadores.
+
+Resultado: 108
+"""
+
+23 + 45 * 2 - 8 // 3
