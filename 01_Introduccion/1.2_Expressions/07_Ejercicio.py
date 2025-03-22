1122288144c136dcf6a116c0b3108b4a13cdb0fd
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-03-22 13:19:25 -0600

    feat: add 07_Ejercicio

---
+++ 01_Introduccion/1.2_Expressions/07_Ejercicio.py
@@ -0,0 +1,16 @@
+"""
+EJERCICIO 7:
+Si a = 8 y b = 3, ¿Cuál es el valor de 
+a > b and a % b = 2
+
+Descripción:
+Expresión boolneana con operadores lógicos
+y de comparación.
+
+Resultado: True
+"""
+
+a = 8 
+b = 3
+
+print(a > b and a % b == 2)
