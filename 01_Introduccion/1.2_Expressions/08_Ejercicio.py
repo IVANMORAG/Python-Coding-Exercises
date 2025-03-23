7345acd6dae76b2f0bde6ff790cabb1706f18e57
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-03-23 10:19:18 -0600

    feat: add 08_Ejercicio

---
+++ 01_Introduccion/1.2_Expressions/08_Ejercicio.py
@@ -0,0 +1,15 @@
+"""
+EJERCICIO 8: 
+Con 'name = Python', evalúa:
+len(name) * 2 > 10
+
+Descripión:
+Expresión que combina función,
+aritmética y comparación.
+
+Resultado: True
+"""
+
+name = 'Python'
+
+print(len(name) * 2 > 10)
