50ec8cfa81a83f807b85e103d3f010e8af7b8ad4
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-03-22 20:00:22 -0600

    feat: add 06_Ejercicio

---
+++ 01_Introduccion/1.2_Expressions/06_Ejercicio.py
@@ -0,0 +1,14 @@
+"""
+EJERCICIO 6:
+Dada 'x = 5', evalúa: 'x * 2 + x // 2 - x % 3
+
+Descripción:
+Expresión con variable y múltiples operadores.
+
+Resultado: 10
+"""
+
+x = 5
+
+operacion = x * 2 + x // 2 - x % 3
+print(operacion)
