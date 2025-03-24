62552766940cf7447540db3e22aa07e220b5b97d
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-03-23 23:59:14 -0600

    feat: add 09_Ejercicio

---
+++ 01_Introduccion/1.2_Expressions/09_Ejercicio.py
@@ -0,0 +1,15 @@
+"""
+EJERCICIO 9:
+Dada 'temp = 25'. evalúa: 
+temp => 20 and temp <= 30
+
+Descripción:
+Expresión para verificar si un valor está 
+en un rango.
+
+Resultado: True
+"""
+
+temp = 25
+
+print(temp >= 20 and temp <= 30)
