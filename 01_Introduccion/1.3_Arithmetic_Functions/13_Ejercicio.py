2de35bd32f3961eb1aa5e573aac13fbb74a07263
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-03-25 08:51:36 -0600

    feat: add 13_Ejercicio

---
+++ 01_Introduccion/1.3_Arithmetic_Functions/13_Ejercicio.py
@@ -0,0 +1,13 @@
+"""
+EJERCICIO 13
+¿Qué devuelve 'max(10, 5, 20, 3)' y
+min(10, 5, 20, 3)'?
+
+Descripción:
+Funciones para encontrar valores máximo y mínimo.
+
+Resultado: 20 y 3
+"""
+
+print(max(10, 5, 20, 3))
+print(min(10, 5, 20, 3))
