e1dbebac70c05a890c338de82b2f675aa5c5292b
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-03-24 20:13:35 -0600

    feat: add 10_Ejercicio

---
+++ 01_Introduccion/1.2_Expressions/10_Ejercicio.py
@@ -0,0 +1,15 @@
+"""
+EJERCICIO 10
+Si 'x = 4' y ' y = 7', calcula: 
+(x + y) ** 2 != x ** 2 + y ** 2
+
+Descripción:
+Comparación de expresiones matemáticas.
+
+Resultado: True
+"""
+
+x = 4
+y = 7
+
+print((x + y) ** 2 != x ** 2 + y ** 2)
