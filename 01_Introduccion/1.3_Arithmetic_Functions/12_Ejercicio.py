8695fa8480da709ad983a4ac2ba126dfb02fa927
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-03-25 01:07:18 -0600

    feat: add 12_Ejercicio

---
+++ 01_Introduccion/1.3_Arithmetic_Functions/12_Ejercicio.py
@@ -0,0 +1,12 @@
+"""
+Calcula 'round(3.1416, 2) y 'round(3.73465)'.
+
+Descripción:
+Función de redondeo con y sin especificar decimales.
+
+Resultado: 3.14 y 3
+"""
+
+print(round(3.1416, 2))
+print("\n")
+print(round(3.73465))
