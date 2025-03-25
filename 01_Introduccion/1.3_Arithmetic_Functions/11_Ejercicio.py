1e76a4f5e9a64d5ae6c8727742f3ed7da9f51b48
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-03-24 19:36:27 -0600

    feat: add 11_Ejercicio

---
+++ 01_Introduccion/1.3_Arithmetic_Functions/11_Ejercicio.py
@@ -0,0 +1,15 @@
+"""
+EJERCICIO 11:
+¿Cuál es el resultado de 'abs(-15)' y 
+'abs(15)'?
+
+Descripción:
+Función de valor absoluto con números positivos y 
+negativos.
+
+Resultado: 15 y 15
+"""
+
+print(f"Valor absoluto de 15: {abs(15)}")
+print("\n")
+print(f"Valor absoluto de '-15': {abs(-15)}")
