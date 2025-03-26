5766cb9ebc14edfdc595f67a35b696417ac41c8f
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-03-26 01:01:31 -0600

    feat: add 14_Ejercicio

---
+++ 01_Introduccion/1.3_Arithmetic_Functions/14_Ejercicio.py
@@ -0,0 +1,12 @@
+"""
+EJERCICIO 14:
+Evalúa pow(2, 8) y pow(27, 1/3).
+
+Descripción: Función de potencia con 
+exponente entero y fraccionario.
+
+Resultado: 256 y 3.0
+"""
+
+print(pow(2,8)) 
+print(pow(27,1/3))
