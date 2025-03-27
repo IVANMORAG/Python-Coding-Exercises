4f7cbd6a9e781bca97fb3bfb075efb64b3d647eb
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-03-27 07:43:54 -0600

    feat: add 17_Ejercicio

---
+++ 01_Introduccion/1.4_Strings/17_Ejercicio.py
@@ -0,0 +1,16 @@
+"""
+EJERCICIO 17:
+Con palabra = "Python", evalúa 
+palabra.lower() y palabra.upper().
+
+Descripción:Métodos para cambiar mayúsculas 
+y minúsculas.
+
+Resultado:
+"python" y "PYTHON"
+"""
+
+palabra = "Python"
+
+print(palabra.lower())
+print(palabra.upper())
