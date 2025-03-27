200ae09ce4c3a2287df43b3c2439054ae8400b16
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-03-26 18:43:26 -0600

    feat: add 16_Ejercicio

---
+++ 01_Introduccion/1.4_Strings/16_Ejercicio.py
@@ -0,0 +1,14 @@
+"""
+EJERCICIO 16:
+Si 'texto = "Hola mundo"', ¿qué devuelve
+texto[5:].
+
+Descripción:
+Slicing de string desde un índice especifco.
+
+Resultado: "Mundo"
+"""
+
+texto = "Hola Mundo"
+
+print(texto[5:])
