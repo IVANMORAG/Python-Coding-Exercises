d011f8b5a9e40c0c3d5f056723c758cfe30049ab
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-03-26 20:09:52 -0600

    feat: add 15_Ejercicio

---
+++ 01_Introduccion/1.3_Arithmetic_Functions/15_Ejercicio.py
@@ -0,0 +1,11 @@
+"""
+EJERCICIO 5:
+¿Cuál es el resultado de 'sum([1, 2, 3, 4, 5])?
+
+Descripción:
+Función suma aplicada a una lista de números.
+
+Resultado: 15
+"""
+
+print(sum([1, 2, 3, 4, 5]))
