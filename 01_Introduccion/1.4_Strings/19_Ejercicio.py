6f2d226a9565836ada584a03b6304e434f4b14a9
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-03-27 02:30:33 -0600

    feat: add 19_Ejercicio

---
+++ 01_Introduccion/1.4_Strings/19_Ejercicio.py
@@ -0,0 +1,14 @@
+"""
+EJERCICIO 19:
+Si frase = "Me gusta programar."
+¿qué devuelve frase.split()?
+
+Descripción:
+Método split para dividir strings en una lista.
+
+Resultado: ['Me', 'gusta', 'programar']
+"""
+
+frase = "Me gusta programar"
+
+print(frase.split())
