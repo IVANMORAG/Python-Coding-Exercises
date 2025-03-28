cded2c58accbf6c90ccd270940fe58719e2e8798
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-03-28 10:24:02 -0600

    feat: add 20_Ejercicio

---
+++ 01_Introduccion/1.4_Strings/20_Ejercicio.py
@@ -0,0 +1,15 @@
+"""
+Ejercicio 20:
+Evalúa "10" + "20" vs int("10") + int("20").
+
+Descripción:
+Concatenación de strings vs suma de enteros 
+convetidos.
+
+Resultado:
+"1020" vs 30
+"""
+
+print("10" + "20")
+
+print(int("10") + int("20"))
