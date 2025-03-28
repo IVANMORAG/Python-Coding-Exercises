17c7a8948ba5c96680073d6db0d97ed4ae2b4dbc
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-03-28 15:35:28 -0600

    feat: add 21_Ejercicio

---
+++ 01_Introduccion/1.5_Values_and_Types/21_Ejercicio.py
@@ -0,0 +1,17 @@
+"""
+EJERCICIO 21:
+¿Cuál es el tipo de cada valor?
+42, 3.14, "Hello", True
+
+Descripción:
+Identificar tipos de datos básicos.
+
+Resultado: int, float, str, bool
+"""
+print(type(42))
+
+print(type(3.14))
+
+print(type("Hello"))
+
+print(type(True))
