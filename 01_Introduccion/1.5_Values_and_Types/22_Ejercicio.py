c3f8a16fd7708d0f8702f4c9b0954069a8e6c676
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-03-29 14:07:23 -0600

    feat: add 22_Ejercicio

---
+++ 01_Introduccion/1.5_Values_and_Types/22_Ejercicio.py
@@ -0,0 +1,14 @@
+"""
+EJERCICIO 22:
+¿Qué devuelve type(5/2) y type(5b // 2)?
+
+Descripción:
+Tipos resultantes de división normal vs división
+entera.
+
+Resultado: float, int
+"""
+
+print(type(5 / 2))
+
+print(type(5 // 2))
