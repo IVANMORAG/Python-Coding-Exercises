ce45f00656d2222492504fa7618584944fa260e7
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-03-30 12:47:39 -0600

    feat: add 24_Ejercicio

---
+++ 01_Introduccion/1.5_Values_and_Types/24_Ejercicio.py
@@ -0,0 +1,11 @@
+"""
+EJERCICIO 24:
+¿Cuál es el resultdo de str(123) + str(456)?
+
+Descripción:
+Conversión de números a string y concatenación.
+
+Resultado: "123456"
+"""
+
+print(str(123) + str(456))
