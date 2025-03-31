29f79f9e0a846e3627baa5469dc6924c788ba44e
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-03-31 05:55:22 -0600

    feat: add 26_Ejercicio

---
+++ 02_Funciones/2.1_Definiing_New_Functions/26_Ejercicio.py
@@ -0,0 +1,13 @@
+"""
+EJERCICIO 26:
+Defie una función llamada saludar() que 
+no reciba parametros y retorne "Hola mundo"
+
+Descripción: 
+Crear una función básica sin parámetros.
+"""
+
+def saludar():
+    return "Hola mundo"
+
+print(saludar())
