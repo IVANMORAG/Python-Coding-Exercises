849dc3265ea7a4b4736fe379a604ea1dafe12094
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-03-30 20:07:44 -0600

    feat: add 23_Ejercicio

---
+++ 01_Introduccion/1.5_Values_and_Types/23_Ejercicio.py
@@ -0,0 +1,19 @@
+"""
+EJERCICIO 23:
+Evalúa bool(0), bool(""), bool("False"), 
+bool([]).
+
+Descripción:
+Conversión de diferentes valores a booleano.
+
+Resultado: False, False, True, False
+(Strig "False no esta vacío, por eso es True")
+"""
+
+print(bool(0))
+
+print(bool(""))
+
+print(bool("False"))
+
+print(bool([]))
