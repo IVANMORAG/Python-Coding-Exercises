53f9679cfb244208db5c29426b4801e59e9b0178
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-03-31 07:10:33 -0600

    feat: add 25_Ejercicio

---
+++ 01_Introduccion/1.5_Values_and_Types/25_Ejercicio.py
@@ -0,0 +1,13 @@
+"""
+EJERCICIO 25:
+¿Qué devuelve isinstance(3.0, int) y 
+isinstance(3.0, float)?
+
+Descripción:
+Verificar si un valor de un tipo especifíco.
+
+Resultado: False y True
+"""
+
+print(isinstance(3.0, int))
+print(isinstance(3.0, float))
