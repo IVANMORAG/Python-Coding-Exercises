b5ae948b3146740e42312686c137f2d3b8106607
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-04-01 00:13:25 -0600

    feat: add 29_Ejercicio

---
+++ 02_Funciones/2.1_Definiing_New_Functions/29_Ejercicio.py
@@ -0,0 +1,13 @@
+"""
+EJERCICIO 29:
+Crea una función mayor_de_tres() que
+reiba tres números y retorne el mayor.
+
+Descripción:
+Función que compara múltiples valores.
+"""
+
+def mayor_de_tres(a, b, c):
+    return max(a, b, c)
+
+mayor_de_tres(2, 90, 1)
