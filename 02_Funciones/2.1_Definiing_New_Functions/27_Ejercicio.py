4f744a4cccdfd9c143c98b4a281e776a047feef3
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-04-01 16:32:14 -0600

    feat: add 27_Ejercicio

---
+++ 02_Funciones/2.1_Definiing_New_Functions/27_Ejercicio.py
@@ -0,0 +1,13 @@
+"""
+EJERCICIO 27:
+Crea una función calcular_area_rectangulo(), que
+reciba ancho y largo como parámetros y retorne el área.
+
+Descripción:
+Fución con multiples parámetros y cálculo.
+"""
+
+def calcular_area_rectangulo(ancho, largo):
+    return largo * ancho
+
+calcular_area_rectangulo(10,5)
