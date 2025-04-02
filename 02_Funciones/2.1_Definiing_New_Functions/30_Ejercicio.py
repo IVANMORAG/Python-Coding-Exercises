ac711443e08c7a01d4ba609a305b86cbee206e5d
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-04-02 10:10:44 -0600

    feat: add 30_Ejercicio

---
+++ 02_Funciones/2.1_Definiing_New_Functions/30_Ejercicio.py
@@ -0,0 +1,12 @@
+"""
+EJERCICIO 30
+Define una función convertir_temperatura() que .
+convieta de Celsius a Fahrenheit(F = C * 9/5 + 32)
+
+Descripción: Función con fórmula matemática especifíca.
+"""
+
+def convertir_temperatura(celcius):
+    return celcius * 9/5 + 32
+
+convertir_temperatura(40)
