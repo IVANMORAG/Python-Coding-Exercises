650574169951e0e40b69dc1324b7b2952e60702e
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-04-02 21:00:36 -0600

    feat: add 31_Ejercicio

---
+++ 02_Funciones/2.2_Parameters/31_Ejercicio.py
@@ -0,0 +1,14 @@
+"""
+EJERCICIO 31:
+Escribe una función potencia, que reciba 
+una base y un exponente, y retorne base 
+elevada al exponente.
+
+Descripción:
+Función con dos parámetros numéricos.
+"""
+
+def potencia(base, exponente):
+    return base ** exponente
+
+potencia(2, 3)
