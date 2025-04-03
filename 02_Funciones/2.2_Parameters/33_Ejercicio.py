d79a924c250d2e0f640d5c6897660ed4da24f8c7
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-04-03 15:11:57 -0600

    feat: add 33_Ejercicio

---
+++ 02_Funciones/2.2_Parameters/33_Ejercicio.py
@@ -0,0 +1,14 @@
+"""
+EJERCICIO 33:
+Define una función es_mayor, que reciba dos
+números y retorne True si el primero es mayor que el 
+segundo.
+
+Descripción:
+Función que retorna un booleano.
+"""
+
+def es_mayor(num1, num2):
+    return num1 > num2
+
+es_mayor(10, 1)
