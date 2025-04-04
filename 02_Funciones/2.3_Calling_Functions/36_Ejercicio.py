4e4047aaf2c04f3ed2adedc69ed8bccda2146fd7
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-04-04 15:49:01 -0600

    feat: add 36_Ejercicio

---
+++ 02_Funciones/2.3_Calling_Functions/36_Ejercicio.py
@@ -0,0 +1,16 @@
+"""
+EJERCICIO 36:
+Usando la función doble, del ejercicio anterior,
+escribe código que llame a la función con el número 15
+y guarde el resultado en una variable llamada resultado.
+
+Descripción:
+Llamar función y asignar resultado.
+"""
+
+def doble(numero):
+    return numero * 2
+
+resultado = doble(15)
+
+print(resultado)
