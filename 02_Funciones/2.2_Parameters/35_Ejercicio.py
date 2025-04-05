4c14adf930e124072b082fa7c2af0ea352bee6db
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-04-04 18:43:50 -0600

    feat: add 35_Ejercicio

---
+++ 02_Funciones/2.2_Parameters/35_Ejercicio.py
@@ -0,0 +1,13 @@
+"""
+EJERCICIO 35:
+Crea una función promedio_tres, que reciba
+tres números y retorne su promedio.
+
+Descripción:
+Función que suma tres números y divide entre 3.
+"""
+
+def promedio_tres(a, b, c):
+    return (a + b + c) / 3
+
+promedio_tres(1, 2, 3)
