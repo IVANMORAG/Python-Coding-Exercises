7450df528e26e8f94d1f8c657e58d5ae2a9c2919
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-04-04 21:08:27 -0600

    feat: add 34_Ejercicio

---
+++ 02_Funciones/2.2_Parameters/34_Ejercicio.py
@@ -0,0 +1,13 @@
+"""
+EJERCICIO 34:
+Escribe una función es_positivo, que reciba 
+un número y retorne True si es mayor que 0.
+
+Descripción:
+Función que evalúa una condición simple.
+"""
+
+def es_positivo(numero):
+    return numero > 0
+
+es_positivo(1)
