9fd2ba335edbc27cf39e708bf90350795ef789e8
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-04-06 04:03:21 -0600

    feat: add 39_Ejercicio

---
+++ 02_Funciones/2.3_Calling_Functions/39_Ejercicio.py
@@ -0,0 +1,15 @@
+"""
+EJERCICIO 39:
+Escribe codigo que use es_mayor para
+comparar los números 25 y 18, y guarde el resultado
+en comparación.
+
+Descripción:
+Llamar función que retorna booleano.
+"""
+
+def es_mayor(num1, num2):
+    return num1 > num2
+
+comparacion = es_mayor(25, 18)
+print(comparacion)
