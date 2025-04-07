cc120ab033c5b40207d41a2a4626140b975d43e3
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-04-07 01:12:40 -0600

    feat: add 41_Ejercicio

---
+++ 02_Funciones/2.4_Repetition/41_Ejercicio.py
@@ -0,0 +1,15 @@
+"""
+EJERCICIO 41:
+Escribe una función contar_del_1_al_5 que
+imprima los números del 1 al 5, uno por linea.
+
+Descripción:
+Función con bucle básico usando range.
+"""
+
+def contar_del_1_al_5():
+    for i in range(1, 6):
+        print(i)
+        
+        
+contar_del_1_al_5()
