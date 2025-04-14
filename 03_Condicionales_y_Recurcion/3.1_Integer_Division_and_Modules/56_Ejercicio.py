92e149e5500c045aa6b1d6e57912f15ecf401e11
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-04-14 17:01:19 -0600

    feat: add 56_Ejercicio

---
+++ 03_Condicionales_y_Recurcion/3.1_Integer_Division_and_Modules/56_Ejercicio.py
@@ -0,0 +1,13 @@
+"""
+EJERCICIO 56:
+Escribe una función dividir_enteros, que reciba dos
+números y retorne solo la parte entera de la dvisió.
+
+Descripción:
+Usar el operador // para divisón entera.
+"""
+
+def dividir_eneros(a, b):
+    return a // b
+
+print(dividir_eneros(5, 4))
