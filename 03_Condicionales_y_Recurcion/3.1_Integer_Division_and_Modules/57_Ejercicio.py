253df3fc38bf6372e430b95c5a352c89af653e6c
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-04-14 07:08:51 -0600

    feat: add 57_Ejercicio

---
+++ 03_Condicionales_y_Recurcion/3.1_Integer_Division_and_Modules/57_Ejercicio.py
@@ -0,0 +1,13 @@
+"""
+EJERCICIO 57:
+Crea una función obtener_resto que reciba dos números
+y retorne el resto de la división.
+
+Descripción:
+Usar el operador % para obtener el módulo.
+"""
+
+def obtener_resto(a, b):
+    return a % b
+
+print(obtener_resto(10, 2))
