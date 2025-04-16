27031f265c158322e054eafabce1aa9f3d804d0f
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-04-16 13:55:53 -0600

    feat: add 61_Ejercicio

---
+++ 03_Condicionales_y_Recurcion/3.2_Boolean_Expressions/61_Ejercicio.py
@@ -0,0 +1,14 @@
+"""
+EJERCICIO 61:
+Escribe una función es_mayor_que_diez quereciba
+un número y retorne True si es mayor.
+
+Descripción:
+Expresión booleana simple con comparación.
+"""
+
+def es_mayor_que_diez(numero):
+    return numero > 10
+
+print(es_mayor_que_diez(20))
+
