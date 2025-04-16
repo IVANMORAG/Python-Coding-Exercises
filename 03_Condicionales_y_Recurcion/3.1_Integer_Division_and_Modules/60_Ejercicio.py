e617faf5cdc2982ec239a4444d233fb67a911659
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-04-16 01:05:28 -0600

    feat: add 60_Ejercicio

---
+++ 03_Condicionales_y_Recurcion/3.1_Integer_Division_and_Modules/60_Ejercicio.py
@@ -0,0 +1,13 @@
+"""
+EJERCICIO 60:
+Crea una función es_par_con_modulo que use el operador 
+% para determinar si un número es par.
+
+Descripción:
+Un número es par si un numero % 2 == 0.
+"""
+
+def es_par_con_modulo(numero):
+    return numero % 2 == 2
+
+print(es_par_con_modulo(78))
