6f8cc32951c6046c0d9add02b86a1786f0fe09dd
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-04-16 17:38:07 -0600

    feat: add 62_Ejercicio

---
+++ 03_Condicionales_y_Recurcion/3.2_Boolean_Expressions/62_Ejercicio.py
@@ -0,0 +1,13 @@
+"""
+EJERCICIO 62:
+Crea una función son_iguales que reciba dos 
+números y retorne True si son exactamente iguales.
+
+Descripción:
+Usar el operador == para comparación de igualdad.
+"""
+
+def son_iguales(num1, num2):
+    return num1 == num2
+
+print(son_iguales(12, 12))
