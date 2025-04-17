b558697f3fa9af3fcec687ec0203b516a1640c39
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-04-17 07:59:20 -0600

    feat: add 65_Ejercicio

---
+++ 03_Condicionales_y_Recurcion/3.2_Boolean_Expressions/65_Ejercicio.py
@@ -0,0 +1,13 @@
+"""
+EJERCICIO 65:
+Crea una función es_string_vacio que reciba un string
+y retorne True si esta vacio.
+
+Descripción:
+Comparar string con string vacio.
+"""
+
+def es_string_vacio(palabra):
+    return palabra == ""
+
+print(es_string_vacio("Hola"))
