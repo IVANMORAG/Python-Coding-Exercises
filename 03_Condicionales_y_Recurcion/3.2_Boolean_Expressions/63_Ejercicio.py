5144bd4bfa4bc1f7d8faae3617096140a8347397
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-04-17 22:06:22 -0600

    feat: add 63_Ejercicio

---
+++ 03_Condicionales_y_Recurcion/3.2_Boolean_Expressions/63_Ejercicio.py
@@ -0,0 +1,13 @@
+"""
+EJERCICIO 63:
+Define una función es_diferente_de_cero que reciba
+un número y retorne True, sino es cero.
+
+Descripción:
+Usar el operador != para comparación de desigualdad.
+"""
+
+def es_diferente_de_cero(numero):
+    return numero != 0
+
+print(es_diferente_de_cero(0))
