2d04f01d1d7c7798eb04ad8c2db93dec26c3bfac
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-04-19 07:15:56 -0600

    feat: add 68_Ejercicio

---
+++ 03_Condicionales_y_Recurcion/3.3_Logical_Operators/68_Ejercicio.py
@@ -0,0 +1,13 @@
+"""
+EJERCICIO 68:
+Define una función no_es_cero, que usa el operador not, 
+para verificar que un número no sea cero.
+
+Descripción:
+Usa el operador not para negar una condición.
+"""
+
+def no_es_cero(numero):
+    return not numero == 0
+
+print(no_es_cero(7))
