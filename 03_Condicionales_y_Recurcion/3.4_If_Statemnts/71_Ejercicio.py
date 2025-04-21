f452f8f52b8a6886b01de7e974f649c9951847b8
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-04-21 01:24:26 -0600

    feat: add 71_Ejercicio

---
+++ 03_Condicionales_y_Recurcion/3.4_If_Statemnts/71_Ejercicio.py
@@ -0,0 +1,16 @@
+"""
+EJERCICIO 71:
+Escribe una función mensaje_si_positivo que reciba 
+un número e imprima "Es positivo" solo si el número
+es mayor a 0
+
+Descripción:
+If statemet basico sin else.
+"""
+
+def mensaje_si_positivo(numero):
+    if numero > 0:
+        print("Es positvo")
+        
+        
+mensaje_si_positivo(5)
