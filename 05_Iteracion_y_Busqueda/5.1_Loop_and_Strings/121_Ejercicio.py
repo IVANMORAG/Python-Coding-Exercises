2604af81b954385be7937a3e50b4d9fe0c2f7e3f
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-05-13 22:58:02 -0600

    feat: add 121_Ejercicio

---
+++ 05_Iteracion_y_Busqueda/5.1_Loop_and_Strings/121_Ejercicio.py
@@ -0,0 +1,15 @@
+"""
+EJERCICIO 121:
+Escribe una función imprimir_caracteres que reciba un string e imprima 
+cada caracter en una sola linea separada.
+
+Descripción: 
+Bucle básico para recorrer string carácter por carácter
+"""
+
+def imprimir_caracter(texto):
+    for letra in texto:
+        print(letra)
+        
+imprimir_caracter("Tacos al pastor")
+        
