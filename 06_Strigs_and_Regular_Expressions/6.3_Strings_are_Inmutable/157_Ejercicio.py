ff3465f64895b0cf9b2706b7551c2df7943945fc
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-08-05 22:23:08 -0600

    feat: add 157_Ejercicio

---
+++ 06_Strigs_and_Regular_Expressions/6.3_Strings_are_Inmutable/157_Ejercicio.py
@@ -0,0 +1,14 @@
+"""
+EJERCICIO 157:
+Crea una función agregar_al_final  que retorne un nuevo string con un
+caracter agregado al final.
+
+Descripión:
+Concatenación para modificar string.
+"""
+
+def agregar_al_final(texto, agregar):
+    
+    return texto + agregar
+
+print(agregar_al_final("Messi", "Cr7"))
