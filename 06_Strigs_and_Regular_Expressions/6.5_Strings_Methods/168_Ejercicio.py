f0cd932c4dbd242693702289845926abc1775d55
IVANMORAG <ivanmoragarcia412@gmail.com>
2025-08-05 23:49:58 -0600

    feat: add 168_Ejercicio

---
+++ 06_Strigs_and_Regular_Expressions/6.5_Strings_Methods/168_Ejercicio.py
@@ -0,0 +1,14 @@
+"""
+EJERCICI 168:
+Define una función capitalizar_palabras que use metodos de string para
+capatalizar la primera letra de cada palabra.
+
+Descripción:
+Usar title() o combinación de split() y capitalize().
+"""
+
+def capitalizar_palabras(texto):
+    return texto.title()
+
+
+print(capitalizar_palabras("hola mundo en python"))
